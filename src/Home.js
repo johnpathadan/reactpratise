868eb3b58e086b984bba8cdb57590b483789281e
johnpathadan <johnpathadan@gmail.com>
2022-07-12 12:28:41 +0530

    Handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,41 @@
-import { useState, useEffect } from "react"; 
+
+import { useEffect, useState } from "react";
 import BlogList from "./Bloglist";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null); 
-    const [isPending, setIsPending] = useState(true);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(()=>{ 
-        //to simulate the loading, we are using setTimneOut around fetch
-        setTimeout(()=>{
-            fetch('http://localhost:8000/blogs')
-            .then(res =>{  
-                
-                return res.json(); 
-                
-            })
-            .then(data=>{
-                setBlogs(data);
-                setIsPending(false); //added code
-            })
-        }, 1000);
-        
-    }, []); 
-    
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogss')
+      .then(res => {
+        if (!res.ok) { // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setBlogs(data);
+        setError(null);
+      })
+      .catch(err => {
+        // auto catches network / connection error
+        setIsPending(false);
+        setError(err.message);
+      })
+    }, 1000);
+  }, [])
 
-    return (
-        <div className="homePage">
-            {/* added code below */}
-            {isPending && <div>Loading...</div>}  
-            { blogs && < BlogList blogs={blogs} title="All Blogs!"/>}
-            
-            
-        </div>
-    );
-
-
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
  
 export default Home;
