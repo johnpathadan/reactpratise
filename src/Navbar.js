687744f8ce5d6b1abbcac7aa39c6648f04af92d3
johnpathadan <johnpathadan@gmail.com>
2022-07-12 16:46:58 +0530

    Router Links 2

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,13 @@
+import { Link } from "react-router-dom"; //added code
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="http://">Home</a>
-                <a href="/create" style={{
-                    color: "white",
-                    backgroundColor: "#f1356d",
-                    borderRadius: "8px"
-                }}>New Blog</a>
+                {/* <a href="http://">Home</a>  change it to below*/ }
+                <Link to="/"></Link> 
+                {/* Link prevents the request to the server */}
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
      );
