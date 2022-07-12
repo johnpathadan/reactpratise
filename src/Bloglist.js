012efda2feeff10e178fbaa27a400a73dc84c94b
johnpathadan <johnpathadan@gmail.com>
2022-07-12 17:55:50 +0530

    Route Parameters

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({blogs, title}) => { 
 
     return ( 
@@ -7,8 +9,11 @@ const BlogList = ({blogs, title}) => {
                 
                 <div className="blog-preview" key={blog.id}>
                     
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
+                    <Link to={`/blogs/${blog.id}`}> 
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                    </Link>
+                    
                 </div>
             ))}
         </div>
