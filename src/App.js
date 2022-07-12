9783a3fd91290090367988a4f12dcc231e667e45
johnpathadan <johnpathadan@gmail.com>
2022-07-12 16:37:06 +0530

    Router Links

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'; //added code
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import Create from './Create';
 
 
 function App() {
   
   return(
-    // <Router> </Router> added code
     <Router>  
     <div className='App'>
-      {/* Navbar will appear for every single route as it is outside Switch */}
       < Navbar/>
       <div className='content'>
-        {/* <Home/>  replaced with Switch, Switch makes sure only 1 route shows at any given time*/}
         <Switch>
-          {/* Let's create a route for the homepage, path varies like path='/aboutus', path='/contact' */}
-          {/* path='/' means initial route */}
-          <Route path='/'>
+          <Route exact path='/'>
             <Home />
+          </Route>
+          <Route path='/create'>
+            <Create/>
           </Route>
         </Switch>
       </div>
@@ -30,25 +29,3 @@ function App() {
 export default App;
 
 
-// import Navbar from './Navbar';
-// import Home from './Home';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Navbar />
-//         <div className="content">
-//           <Switch>
-//             <Route path="/">
-//               <Home />
-//             </Route>
-//           </Switch>
-//         </div>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
